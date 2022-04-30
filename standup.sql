efe2e490a2294a28e1e958179fa88f325220bc75
Brahian Pena <teslasapbe@gmail.com>
2022-04-30 20:36:33 -0300

    fix: initialization failing

---
+++ standup.sql
@@ -0,0 +1,5 @@
+CREATE USER ort_admin IDENTIFIED BY "ort_admin";
+
+
+GRANT ALL PRIVILEGES TO "ort_admin";
+
--- startup.sh
+++ startup.sh
@@ -1,28 +1,51 @@
-#!/bin/bash
-# Uncomment the following statement for debug purposes
-#set -xe
+#!/usr/bin/env bash
+# Variables used for the script
 
-ADMIN_PASS="PassAdm123"
+CMG_COMMAND=docker
+DKR_IMAGE="gvenzl/oracle-xe"
+DKR_HOST_PORT=$1
+DKR_CNT_NAME=$2
+DKR_DATA_PATH=dbcxe-$DKR_CNT_NAME-dvol
+DKR_ORA_USER_APP="ORTSYSDBA"
+DKR_ORA_PW="PassAdm123"
+DKR_ORA_PW_APP="PassAdm123"
 
-# If the container name is being generated dinamically
-# you may have to modify this to use the container ID 
-# (you can read it from the "docker ps" command output)
-CONTAINER_NAME="oracle_ort"
+if [ -z "$DKR_HOST_PORT" ]; then
+  echo "You must provide docker host port"
+  exit 1
+fi
 
-main() {
 
-    # Build and run container dettached
-    docker compose up --build --detach
+if [ -z "$DKR_CNT_NAME" ]; then
+  echo "You must provide dockercontainer name"
+  exit 1
+fi
 
-    # Change admin password
-    docker exec "${CONTAINER_NAME}" ./setPassword.sh "${ADMIN_PASS}"
 
-    echo "================================";
-    echo "ORACLE PASSWORD FOR SYS, SYSTEM AND PDBADMIN: ${ADMIN_PASS}";
-    echo "================================";
+echo "Docker Image ............................ :'$DKR_IMAGE'"
+echo "Database (SYS AND SYSTEM) User Password . :'$DKR_ORA_PW'"
+echo "Database Application User ............... :'$DKR_ORA_USER_APP'"
+echo "Database Application User Password ...... :'$DKR_ORA_PW_APP'"
+echo "Docker Volume Name ...................... :'$DKR_DATA_PATH'"
+echo "Docker Host Port ........................ :'$DKR_HOST_PORT'"
+echo "Docker Container Name ................... :'$DKR_CNT_NAME'"
 
-    # Attach to the standard output of the container
-    docker logs "${CONTAINER_NAME}" --follow
-}
 
-main
+if docker ps -a --format '{{.Names}}' | grep -Eq "^${DKR_CNT_NAME}\$"; then
+   $CMG_COMMAND start "${DKR_CNT_NAME}"
+else
+  $CMG_COMMAND run --name $DKR_CNT_NAME \
+    -p $DKR_HOST_PORT:1521 \
+    -e "ORACLE_PASSWORD=$DKR_ORA_PW" \
+    -e "APP_USER=$DKR_ORA_USER_APP" \
+    -e "APP_USER_PASSWORD=$DKR_ORA_PW_APP" \
+    -v $DKR_DATA_PATH:/opt/oracle/oradata \
+    -d $DKR_IMAGE
+fi
+
+echo "Waiting for container '$DKR_CNT_NAME' to start ... " 
+sleep 10
+
+echo "Docker Container '$DKR_CNT_NAME' has been started on port '$DKR_HOST_PORT'"
+
+$CMG_COMMAND logs  $DKR_CNT_NAME --follow
